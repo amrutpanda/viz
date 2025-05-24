3ec56e018502bd39ee897a64b44a4128bcb93efc
amrutpanda <panda2amrut@gmail.com>
2025-05-24 17:41:59 +0530

    wrote computeTorque function.But its not building. Need to resolve that.

--- primitives/JointTask.cpp
+++ primitives/JointTask.cpp
@@ -10,6 +10,12 @@ namespace Primitives
         _current_position.resize(_nDof);
         _target_position.resize(_nDof);
         _current_velocity.resize(_nDof);
+        _Kp.resize(_nDof);
+        _Kv.resize(_nDof);
+        h.resize(_nDof);
+
+        _Kp_mat = _Kp.asDiagonal();
+        _Kv_mat = _Kv.asDiagonal();
 
         _current_position = _robot_model->_q;
         _current_velocity = _robot_model->_dq;
@@ -17,6 +23,12 @@ namespace Primitives
 
     void JointTask::computeTorque(Eigen::VectorXd& _T)
     {
-
+        // need to update the robot model before calling this
+        // function for correct torque computation.
+        _current_position = _robot_model->_q;
+        _current_velocity = _robot_model->_dq;
+        _robot_model->coriolisPlusGravityForces(h);
+        _T = _robot_model->_M( -_Kp_mat* (_current_position - _target_position) 
+                                - _Kv_mat*(_current_velocity)) + h;
     }
 } // namespace Primitives
--- primitives/JointTask.h
+++ primitives/JointTask.h
@@ -16,6 +16,12 @@ namespace Primitives
         Eigen::VectorXd _current_position;
         Eigen::VectorXd _target_position;
         Eigen::VectorXd _current_velocity;
+        Eigen::VectorXd _Kp;
+        Eigen::VectorXd _Kv;
+        Eigen::MatrixXd _Kp_mat;
+        Eigen::MatrixXd _Kv_mat;
+
+        Eigen::VectorXd h; // it will store the coriolis torque and gravity torque;
         double _goal_tolerance;
         int _nDof;
         bool velocity_saturation_flag = false;    
